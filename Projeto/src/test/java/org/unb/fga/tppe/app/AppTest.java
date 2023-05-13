88fa4e7f6262b04fe47a1e496667373f67ee4825
RafaelBP02 <rafaelbertopereira@gmail.com>
2023-05-13 09:00:34 -0300

    Inicio do projeto de TPPE

---
+++ Projeto/src/test/java/org/unb/fga/tppe/app/AppTest.java
@@ -0,0 +1,20 @@
+package org.unb.fga.tppe.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ Projeto/src/test/java/org/unb/fga/tppe/test/Test.java
@@ -0,0 +1,18 @@
+package org.unb.fga.tppe.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+
+public class Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
